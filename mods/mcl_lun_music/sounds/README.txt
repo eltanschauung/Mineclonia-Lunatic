0b7906d3729e863c49c2ab220e5d56b6008216dc
eltanschauung <eltanschauung@users.noreply.github.com>
2026-03-02 21:47:10 -0500

    Update mods folder with symlinks for parity, gitignore sounds

--- mods/mcl_lun_music/sounds/README.txt
+++
@@ -1,3 +0,0 @@
-Place your ambient music `.ogg` files here.
-
-Then reference them by filename (without `.ogg`) from `mods/mcl_lun_music/tracks.lua`.
--- mods/mcl_lun_music/tracks.lua
+++
@@ -1,7 +0,0 @@
-return {
-	{id = "haggstrom", sound = "mcl_lun_music_haggstrom", length = 157, weight = 1, allowed_contexts = {dimension = {"overworld"}}},
-	{id = "key", sound = "mcl_lun_music_key", length = 65, weight = 1, allowed_contexts = {dimension = {"overworld"}}},
-	{id = "mice_on_venus", sound = "mcl_lun_music_mice_on_venus", length = 282, weight = 1, allowed_contexts = {dimension = {"overworld"}}},
-	{id = "minecraft", sound = "mcl_lun_music_minecraft", length = 254, weight = 1, allowed_contexts = {dimension = {"overworld"}}},
-	{id = "subwoofer_lullaby", sound = "mcl_lun_music_subwoofer_lullaby", length = 209, weight = 1, allowed_contexts = {dimension = {"overworld"}}},
-}
